8f16b0a7e9d03577a547b515716a6965f9dc2af3
”Yura <dudaryourko@gmail.com>
2018-03-30 13:27:47 +0300

    add dinamic navbar for our homepage

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -85,3 +85,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+from django.conf import global_settings
+
+TEMPLATE_CONTEXT_PROCESSORS = \
+global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
+'django.core.context_processors.request',
+)
